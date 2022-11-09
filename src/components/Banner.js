241a8e746acfbaa7efd22f0822275da6645187c9
Sharrod Hines <sharrod.hines@braingu.com>
2022-11-09 17:10:46 -0500

    set up TMDB requests along with the banner & nav display

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "../axios";
+import requests from "../requests";
 
 const Banner = () => {
+  const [bannerMovie, setBannerMovie] = useState([]);
+
+  useEffect(() => {
+    const fetchBannerMovies = async () => {
+      const response = await axios.get(requests.fetchNetflixOriginals);
+      const randomNum = Math.floor(
+        Math.random() * response.data.results.length - 1
+      );
+      setBannerMovie(response.data.results[randomNum]);
+    };
+
+    fetchBannerMovies();
+  }, []);
+
   return (
     <header
       className="banner"
       style={{
-        backgroundSize: "cover",
-        backgroundImage:
-          "url(https://torranceca.files.wordpress.com/2019/10/netflix.jpg?w=1400&h=)",
-        backgroundPosition: "center center"
+        backgroundImage: `url('https::/image.tmdb.org/t/p/original${bannerMovie?.backdrop_path}')`
       }}
-    ></header>
+    >
+      <div className="banner-content">
+        <h2 className="banner-title">{bannerMovie?.name}</h2>
+        <div className="banner-actions">
+          <button>Play</button>
+          <button>My List</button>
+        </div>
+        <div className="banner-description">{bannerMovie?.overview}</div>
+      </div>
+    </header>
   );
 };
 
